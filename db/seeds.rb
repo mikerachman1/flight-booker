6b1ab742757c854955cae65ffcb5107be00d04f1
Mike Rachman <mikerachman1@gmail.com>
2022-09-14 11:52:35 -0400

    updated db/seeds.rb to create airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+tables = ActiveRecord::Base.connection.tables - ['schema_migrations']
+
+# In SQLite, there is no TRUNCATE statement, instead, it's
+tables.each do |table|
+  ActiveRecord::Base.connection.execute "DELETE FROM `#{table}`"
+  ActiveRecord::Base.connection.execute "DELETE FROM sqlite_sequence WHERE name='#{table}'"
+end
+
+airports = Airport.create([
+  { code: 'SFO' },
+  { code: 'NYC' },
+  { code: 'PHL' },
+  { code: 'CHI' },
+  { code: 'LAX' }
+])
