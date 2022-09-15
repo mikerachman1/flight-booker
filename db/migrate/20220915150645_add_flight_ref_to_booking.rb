452b05f3bfc81d39d28fd0c154bcce3f4984412c
Mike Rachman <mikerachman1@gmail.com>
2022-09-15 11:09:57 -0400

    added references to db schema ran migrations

---
+++ db/migrate/20220915150645_add_flight_ref_to_booking.rb
@@ -0,0 +1,5 @@
+class AddFlightRefToBooking < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :bookings, :flight, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_143637) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_150802) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_143637) do
   create_table "bookings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "flight_id"
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -37,6 +39,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_143637) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_id"
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
+  add_foreign_key "bookings", "flights"
+  add_foreign_key "passengers", "bookings"
 end
